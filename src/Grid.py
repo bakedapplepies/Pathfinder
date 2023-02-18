2b5ebf6ede855dea59f99f4f708a227c4ed8b183
JohnBach <hoangbachtran2016@gmail.com>
2023-02-19 00:15:27 +0700

    Node customization in progress

--- src/Grid.py
+++ src/Grid.py
@@ -1,10 +1,22 @@
 import pygame
 import numpy
 
+from constants import *
+
 
 class Node(pygame.Rect):
     def __init__(self, x, y, dx, dy):
         super().__init__(x, y, dx, dy)
+        self.color = BLACK
+        self.border = 0
+
+    def setState(self, state: str):
+        if state == "Path":
+            self.color = BLACK
+            self.border = 1
+        elif state == "Wall":
+            self.color = BLACK
+            self.border = 0
 
 
 class Grid(numpy.ndarray):
@@ -14,10 +26,12 @@ class Grid(numpy.ndarray):
     def __new__(self, resolution):
         self.listGrid = []
         self.sideLength = 20
+
+        print(f"res: {resolution[0]}x{resolution[1]}")
         
         for i in range(0, resolution[1], self.sideLength):
             self.listGrid.append([])
             for j in range(0, resolution[0], self.sideLength):
-                self.listGrid[int(i/self.sideLength)].append(Node(i, j, self.sideLength, self.sideLength))
+                self.listGrid[int(i/self.sideLength)].append(Node(j, i, self.sideLength, self.sideLength))
 
         return numpy.array(self.listGrid, dtype=Node)
--- src/Pathfinder.py
+++ src/Pathfinder.py
@@ -3,12 +3,10 @@ import pygame
 import time
 
 from Grid import Grid
+from constants import *
 
 
 RESOLUTION = (1200, 800)
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -19,7 +17,7 @@ class Window:
     def __init__(self):
         # Main window
         pygame.display.set_caption("Pathfinder")
-        self.window = pygame.display.set_mode(RESOLUTION)
+        self.window = pygame.display.set_mode(RESOLUTION, pygame.RESIZABLE)
         
         # Game variables
         self.running = True
@@ -29,9 +27,9 @@ class Window:
         
         # Grid
         self.grid = Grid(RESOLUTION)
-        
+
     def Loop(self):
-        deltaTime = 0
+        deltaTime = 0.0
         begin = time.time()
         while self.running:
             deltaTime = time.time() - begin
@@ -50,6 +48,13 @@ class Window:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.VIDEORESIZE:
+                # self.window = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                self.grid = Grid((event.w, event.h))
+            if pygame.mouse.get_pressed()[0]:
+                # print(pygame.mouse.get_pressed())
+                mouse_pos = pygame.mouse.get_pos()
+                self.grid[mouse_pos[1]/self.grid.sideLength][mouse_pos[0]/self.grid.sideLength].setState("Wall")
                 
     # RENDERING
     def Render(self):
@@ -59,7 +64,7 @@ class Window:
     def DrawGrid(self):
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
-                pygame.draw.rect(self.window, BLACK, self.grid[i][j])
+                pygame.draw.rect(self.window, self.grid[i][j].color, self.grid[i][j], self.grid[i][j].border)
     
 
 if __name__ == "__main__":
